8b50254ed987be44a7ec0461428ef81ff02e9385
Márk Ángyán <angyanmark97@gmail.com>
2019-12-10 13:20:04 +0100

    Top level event no delete button

---
+++ FaultTreeEditor/Converters/TopLevelDeleteToVisibilityConverter.cs
@@ -0,0 +1,36 @@
+﻿using FaultTreeEditor.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace FaultTreeEditor.Converters
+{
+    public class TopLevelDeleteToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var v = value as Element;
+            if (v != null)
+            {
+                if (v.DisplayTitle == "Top level event")
+                {
+                    return Visibility.Collapsed;
+                }
+                else
+                {
+                    return Visibility.Visible;
+                }
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
